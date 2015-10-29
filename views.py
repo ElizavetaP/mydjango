280562002afedb017609bbf8b85b6de3d962d264
Liza <lizap@bk.ru>
2015-10-29 20:22:21 +0400

    views.py

--- views.py
+++ views.py
@@ -141,3 +141,25 @@ def detail(request, poll_id):
 
     return render_to_response('polls/detail.html', {'mydata': mydata,'er': er,'e':e,'dates':dates},
                                context_instance=RequestContext(request))
+
+def results(request, poll_id):
+    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+
+def vote(request, poll_id):
+
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the poll voting form.
+        return render_to_response('polls/detail.html', {
+            'poll': p,
+            'error_message': "You didn't select a choice.",
+        }, context_instance=RequestContext(request))
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
